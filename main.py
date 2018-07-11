66ec0245437de9ab0ca62554b92c61ca8a754ca8
Chema Foces <chemafv27@gmail.com>
2018-07-11 06:06:11 +0200

    Improve main launcher

--- main.py
+++ main.py
@@ -5,11 +5,23 @@ import os
 from os import listdir
 from utils.log import log
 from mtypes.creators.file_factory import FileFactory
-from multiprocessing import Pool, TimeoutError
+from multiprocessing import Pool, Process
 from config import EXCLUDE_ROOT
 from utils.order.organizer import Organizer
 from hachoir.core import config
 config.quiet = True
+## FROM: https://stackoverflow.com/questions/6974695/python-process-pool-non-daemonic
+
+class NoDaemonProcess(Process):
+    # make 'daemon' attribute always return False
+    def _get_daemon(self):
+        return False
+    def _set_daemon(self, value):
+        pass
+    daemon = property(_get_daemon, _set_daemon)
+
+class NonDaemonsPool(Pool):
+    Process = NoDaemonProcess
 
 def work(path):
     log.info("Working {}".format(path))
@@ -21,7 +33,7 @@ def work(path):
     return path
 
 if __name__ == "__main__":
-    ppool = Pool()
+    ppool = NonDaemonsPool()
     result_list = []
 
     for param in sys.argv[1:]:
