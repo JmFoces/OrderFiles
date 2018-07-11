bfc468622abb5ef0942d31520fac4af838554d25
Chema Foces <chemafv27@gmail.com>
2018-07-11 05:50:22 +0200

    Improve main launcher

--- main.py
+++ main.py
@@ -5,46 +5,29 @@ import os
 from os import listdir
 from utils.log import log
 from mtypes.creators.file_factory import FileFactory
-from multiprocessing import Process
+from multiprocessing import Pool, TimeoutError
 from config import EXCLUDE_ROOT
 from utils.order.organizer import Organizer
 from hachoir.core import config
 config.quiet = True
 
 def work(path):
+    log.info("Working {}".format(path))
     ffactory = FileFactory()
     organizer = Organizer()
     mfile = ffactory.create_file(path)
     organizer.organize(mfile)
+    log.info("Finished {}".format(path))
 
 
 if __name__ == "__main__":
-    procs = []
+    ppool = Pool()
+
     for param in sys.argv[1:]:
         try:
-            if param not in EXCLUDE_ROOT:
-                if os.path.isdir(param):
-                    for subdir in listdir(param):
-                        if subdir not in EXCLUDE_ROOT:
-                            p = Process(target=work, args=[os.path.join(param, subdir)])
-                            procs.append(p)
-                else:
-                    p = Process(target=work, args=[param])
-                    procs.append(p)
+            ppool.apply_async(work, (param))
         except Exception as e:
             log.exception(e)
 
-    for p in procs:
-        log.info("Starting proc {0}".format(p.pid))
-        p.start()
-        log.info("waiting for proc {0}".format(p.pid))
-
-    while len(procs) > 0:
-        log.info("Pending tasks {0} ".format(len(procs)))
-        for p in procs:
-            p.join(timeout=10)
-            if not p.is_alive():
-                log.info("Ended task {0}".format(p.pid))
-                procs.remove(p)
-                break
+    ppool.join()
     log.info("Finished all tasks")
