e83c770b492eda55c453ef6c97b3d82ae848054c
Chema Foces <chemafv27@gmail.com>
2018-07-11 06:40:13 +0200

    Improve main launcher

--- main.py
+++ main.py
@@ -3,57 +3,46 @@
 import sys
 import os
 from os import listdir
+from queue import Queue
+
 from utils.log import log
 from mtypes.creators.file_factory import FileFactory
-from multiprocessing import Pool, Process
+import multiprocessing
+from multiprocessing import Process
 from config import EXCLUDE_ROOT
 from utils.order.organizer import Organizer
 from hachoir.core import config
 config.quiet = True
-## FROM: https://stackoverflow.com/questions/6974695/python-process-pool-non-daemonic
-
-
-class NoDaemonProcess(Process):
-    # make 'daemon' attribute always return False
-    def _get_daemon(self):
-        return False
-    def _set_daemon(self, value):
-        pass
-
-    daemon = property(_get_daemon, _set_daemon)
-    
-    def __init__(self, *args, **kwargs):
-        Process.__init__(self, *args, **kwargs)
-
-
-
-class NonDaemonsPool(Pool):
-    Process = NoDaemonProcess
-
 
 def work(path):
-    log.info("Working {}".format(path))
     ffactory = FileFactory()
     organizer = Organizer()
     mfile = ffactory.create_file(path)
     organizer.organize(mfile)
+
+work_queue = Queue()
+def worker_loop():
+    path = work_queue.get()
+    log.info("Ordering {}".format(path))
+    work(path)
     log.info("Finished {}".format(path))
-    return path
 
 
 if __name__ == "__main__":
-    ppool = NonDaemonsPool()
-    result_list = []
+    procs = []
+
+    for i in range(0, multiprocessing.cpu_count()):
+        p = Process(target=worker_loop)
+        procs.append(p)
 
     for param in sys.argv[1:]:
         try:
-            result_list.append(ppool.apply_async(work, args=(param,)))
+            work_queue.put(param)
         except Exception as e:
             log.exception(e)
 
-    for result in result_list:
-        log.info("Finished  {}".format(result.get()))
+    for p in procs:
+        p.start()
 
-    ppool.close()
-    ppool.join()
+    work_queue.join()
     log.info("Finished all tasks")
