a082957d45994fe2eccd82cd35bf8381b929f52a
Chema Foces <chemafv27@gmail.com>
2018-07-11 06:11:44 +0200

    Improve main launcher

--- main.py
+++ main.py
@@ -12,16 +12,24 @@ from hachoir.core import config
 config.quiet = True
 ## FROM: https://stackoverflow.com/questions/6974695/python-process-pool-non-daemonic
 
+
 class NoDaemonProcess(Process):
     # make 'daemon' attribute always return False
     def _get_daemon(self):
         return False
     def _set_daemon(self, value):
         pass
+
     daemon = property(_get_daemon, _set_daemon)
+    
+    def __init__(self, *args, **kwargs):
+        Process.__init__(self, *args, **kwargs)
+
+
 
 class NonDaemonsPool(Pool):
     Process = NoDaemonProcess
+
 
 def work(path):
     log.info("Working {}".format(path))
@@ -31,6 +39,7 @@ def work(path):
     organizer.organize(mfile)
     log.info("Finished {}".format(path))
     return path
+
 
 if __name__ == "__main__":
     ppool = NonDaemonsPool()
